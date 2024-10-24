681d6033bfd7166e5ee0a8e7e2e16cdf9ff601db
devrakibulrahman <devrakibulrahman@gmail.com>
2024-10-24 19:25:28 +0600

    data load successfully  from api.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
 import Header from "./components/Header/Header";
 import BodySection from "./components/BodySection/Body";
 import Footer from "./components/Footer/Footer";
+import { useState } from "react";
 
 const App = () => {
+  //? declare useState for hold data from useEffect ----------------> 
+  const [post, setPost] = useState([]);
+
+  //? fetch data from api.json ---------------->
+  useEffect(() => {
+    fetch('api.json')
+      .then((res) => res.json())
+      .then((data) => setPost(data.posts))
+      .catch((err) => console.log(err))
+  }, []);
+
 
   return (
     //? container --------------->
